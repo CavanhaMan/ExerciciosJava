d7a5d4903f1ca96d5da94e54a50c247b1ab84311
CavanhaMan <cavanhaman@hotmail.com>
2020-05-15 13:12:00 -0300

    Criando estrutura basica do programa

---
+++ Main.java
@@ -0,0 +1,58 @@
+/*
++------------------+
+|Rodrigo CavanhaMan|
+|Exercicios em Java|
++------------------+
+*/
+import java.util.Scanner;
+import java.util.Locale;
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(new Locale("en", "US"));
+		Scanner sc = new Scanner(System.in);
+		
+		float imposto;
+		float salario = sc.nextFloat();
+		
+		if (salario <= 3000) imposto = 0;
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		sc.close();
+	}
+}
+"Renda anual com salário:"
+"Renda anual com prestação de serviço:"
+"Renda anual com ganho de capital:"
+"Gastos médicos:"
+"Gastos educacionais:"
+"RELATÓRIO DE IMPOSTO DE RENDA"
+"CONSOLIDADO DE RENDA:"
+"Imposto sobre salário:"
+"Imposto sobre serviços:"
+"Imposto sobre ganho de capital:"
+"DEDUÇÕES:"
+"Máximo dedutível:"
+"Gastos dedutíveis:"
+"RESUMO:"
+"Imposto bruto total:"
+"Abatimento:"
+"Imposto devido:"
