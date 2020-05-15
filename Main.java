69346a45065fa38853b2596de66ca3d81aee6ee2
CavanhaMan <cavanhaman@hotmail.com>
2020-05-15 13:22:08 -0300

    Decaração variaveis de entrada

--- Main.java
+++ Main.java
@@ -17,42 +17,34 @@ public class Main {
 		if (salario <= 3000) imposto = 0;
 		
 		
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		System.out.println("Renda anual com salário:");
+		float rSalario = sc.nextFloat();
+		System.out.println("Renda anual com prestação de serviço:");
+		float rServico = sc.nextFloat();
+		System.out.println("Renda anual com ganho de capital:");
+		float rCapital = sc.nextFloat();
+		System.out.println("Gastos médicos:");
+		float gMedicos = sc.nextFloat();
+		System.out.println("Gastos educacionais:");
+		float gEducacao = sc.nextFloat();
+		
+		System.out.println("\nRELATÓRIO DE IMPOSTO DE RENDA");
+		System.out.println("\nCONSOLIDADO DE RENDA:");
+		System.out.println("Imposto sobre salário:");
+		System.out.println("Imposto sobre serviços:");
+		System.out.println("Imposto sobre ganho de capital:");
+		
+		System.out.println("\nDEDUÇÕES:");
+		System.out.println("Máximo dedutível:");
+		System.out.println("Gastos dedutíveis:");
+		
+		System.out.println("\nRESUMO:");
+		System.out.println("Imposto bruto total:");
+		System.out.println("Abatimento:");
+		System.out.println("Imposto devido:");
+		
 
 		sc.close();
 	}
 }
-"Renda anual com salário:"
-"Renda anual com prestação de serviço:"
-"Renda anual com ganho de capital:"
-"Gastos médicos:"
-"Gastos educacionais:"
-"RELATÓRIO DE IMPOSTO DE RENDA"
-"CONSOLIDADO DE RENDA:"
-"Imposto sobre salário:"
-"Imposto sobre serviços:"
-"Imposto sobre ganho de capital:"
-"DEDUÇÕES:"
-"Máximo dedutível:"
-"Gastos dedutíveis:"
-"RESUMO:"
-"Imposto bruto total:"
-"Abatimento:"
-"Imposto devido:"
+
