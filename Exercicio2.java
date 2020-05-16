9f7497eac52ff556d100e74d466e6767d53cc769
CavanhaMan <cavanhaman@hotmail.com>
2020-05-16 12:16:23 -0300

    exercicio ok - falta a saida para quem nao atende a empresa

--- Exercicio2.java
+++ Exercicio2.java
@@ -13,8 +13,6 @@ public class Exercicio2 {
 		Locale.setDefault(new Locale("en", "US"));
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.printf("Renda anual com salário:");
-		float salario = sc.nextFloat();
 		System.out.printf("Qual sua escolaridade?\n");
 		System.out.printf("   1) Ensino Fundamental");
 		System.out.printf("   2) Ensino Médio      \n");
@@ -22,8 +20,8 @@ public class Exercicio2 {
 		System.out.printf("   4) Pós-graduação     \n");
 		System.out.printf("   Digite uma opção: ");
 		int escolaridade = sc.nextInt();
-		System.out.printf("Você tem quantos anos de experiência profissional? ");
-		double experiencia = sc.nextDouble();
+		System.out.printf("\nVocê tem quantos anos de experiência profissional? ");
+		int experiencia = sc.nextInt();
 		System.out.printf("Você tem disponibilidade para viajar (S/N)? ");
 		char viajar = sc.next().charAt(0);
 		System.out.printf("Você tem habilitação de motorista (S/N)? ");
@@ -34,7 +32,7 @@ public class Exercicio2 {
 		else if (escolaridade == 2) pEscolaridade=20;	//ensino médio 
 		else if (escolaridade == 3) pEscolaridade=30;	//superior
 		else if (escolaridade == 4) pEscolaridade=40;	//pos-graduacao
-		System.out.printf("Pontos por escolaridade: %d\n", pEscolaridade);
+		System.out.printf("\nPontos por escolaridade: %d\n", pEscolaridade);
 		
 		int pExperiencia=0;
 		if (experiencia == 0) pExperiencia = 0;							//0
@@ -46,11 +44,11 @@ public class Exercicio2 {
 		/* Assistentes precisam ter pelo menos ensino médio, e habilitação de motorista.*/
 		/*Gerentes precisam ter acima de 2 anos de experiência e pelo menos ensino superior./
 		/*Analistas precisam ter acima de 5 anos de experiência, disponibilidade para viajar, e pelo menos ensino superior.*/
-		System.out.println("Você está habilitado para o(s) seguinte(s) cargo(s):");
+		System.out.println("\nVocê está habilitado para o(s) seguinte(s) cargo(s):");
 		if (pEscolaridade>=20 && (motorista == 'S' || motorista =='s')) System.out.println("ASSISTENTE");
 		else if (pEscolaridade>=30 && experiencia > 2) System.out.println("GERENTE");
 		else if (pEscolaridade>=30 && experiencia > 5 && (viajar=='s' || viajar=='S')) System.out.println("ANALISTAS");
-		else System.out.println("Infelizmente seu perfil não atende a empresa");
+		else System.out.println("\nInfelizmente seu perfil não atende a empresa");
 		
 		
 		sc.close();
